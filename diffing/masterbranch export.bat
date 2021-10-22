c3c8a1ad1983908881eb458f02ef2adb3a67a840
Nils Lindemann <nilslindemann@tutanota.com>
2021-10-23 01:11:19 +0200

    Add diffing batches and Python script

---
+++ diffing/masterbranch export.bat
@@ -0,0 +1,21 @@
+echo off
+
+echo ********************************************
+echo Please make sure the master branch is active
+echo ********************************************
+
+echo Export the "tw5.com" wiki to a single-file wiki (takes a few seconds) ...
+node ../../TiddlyWiki5/tiddlywiki.js ../../TiddlyWiki5/editions/tw5.com --rendertiddler $:/core/save/all "masterbranch singlefile.html" text/plain
+echo ... done
+
+echo Also export it to a static html file (takes a few more seconds) ...
+node ../../TiddlyWiki5/tiddlywiki.js ../../TiddlyWiki5/editions/tw5.com --rendertiddler $:/core/templates/alltiddlers.template.html "masterbranch static.html" text/plain
+echo ... done
+
+echo Move the exported files to this folder ...
+move ..\..\TiddlyWiki5\editions\tw5.com\output\*.* . > NUL
+rmdir ..\..\TiddlyWiki5\editions\tw5.com\output
+echo ... done
+
+echo DONE
+timeout /t 3 > NUL
