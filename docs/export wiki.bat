9824be2cf05b4c40649f74ae5bd7df8ee8cda773
Nils Lindemann <nilslindemann@tutanota.com>
2021-10-25 22:06:39 +0200

    Remove unnecessary path to wiki
    Because it defaults to the current folder

--- docs/export wiki.bat
+++ docs/export wiki.bat
@@ -7,11 +7,11 @@ echo Please make sure the "UNIFIED-HTML-SYNTAX" branch is active
 echo ***********************************************************
 
 echo Export to a single-file wiki ...
-node ../../TiddlyWiki5/tiddlywiki.js ../docs --rendertiddler $:/core/save/all singlefile.html text/plain
+node ../../TiddlyWiki5/tiddlywiki.js --rendertiddler $:/core/save/all singlefile.html text/plain
 echo ... done
 
 REM  (NOTE) ~ echo Export to a static html file ...
-REM  (NOTE) ~ node ../../TiddlyWiki5/tiddlywiki.js ../docs --rendertiddler $:/core/templates/alltiddlers.template.html static.html text/plain
+REM  (NOTE) ~ node ../../TiddlyWiki5/tiddlywiki.js --rendertiddler $:/core/templates/alltiddlers.template.html static.html text/plain
 REM  (NOTE) ~ echo ... done
 
 echo Move the exported file to this folder ...
--- docs/run wiki via nodejs.bat
+++ docs/run wiki via nodejs.bat
@@ -1,3 +1,3 @@
 echo off
 echo RUN "DOCS" WIKI VIA NODE.JS
-node ../../TiddlyWiki5/tiddlywiki.js ../docs --listen
+node ../../TiddlyWiki5/tiddlywiki.js --listen
