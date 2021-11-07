3cd3d321f0e62076b81eecc206891928c6e92741
Nils Lindemann <nilslindemann@tutanota.com>
2021-11-07 17:04:08 +0100

    Add "log differences" functionality

---
+++ diffing/log differences.py
@@ -0,0 +1,59 @@
+# (OPEN) :Transcludes\: 3
+# (GOTO) :lib:file\:///C\:/run/code/Transcluder/lib.py
+
+from pathlib import Path
+
+# (OPEN) :Path
+# Imports the often used pathlib.Path
+# (CLOSE)
+# (OPEN) :contents
+def contents(path, *args, encoding='utf-8', **kwargs):
+	with path.open('r', *args, encoding=encoding, **kwargs) as f:
+		return f.read()
+# (CLOSE)
+# (OPEN) :realpath
+def realpath(path_string):
+	return Path (path_string).resolve(strict=True)
+# (CLOSE)
+# (CLOSE)
+
+# (NOTE) Quick and dirty script which counts and logs the differences between
+# (NOTE) `featurebranch static normalized.html` and `masterbranch static normalized.html`.
+# (NOTE) For this to work, the files have to be normalized, so that identical lines have
+# (NOTE) the same line number. Currently this will be the case if you set
+# (NOTE) `line_by_line_comparable` to True in `normalize for diffing.py` (append
+# (NOTE) ` loggable` to the file names).
+
+f = contents (realpath ('featurebranch static normalized loggable.html'), encoding="mbcs").splitlines()
+m = contents (realpath ('masterbranch static normalized loggable.html'), encoding="mbcs").splitlines()
+
+len_f = len(f)
+len_m = len(m)
+
+if len_f == len_m:
+
+	print(f'Both files have {len_f} lines')
+
+	differences = {}
+	for counter, f_line in enumerate(f):
+		m_line = m[counter]
+		if m_line != f_line:
+			diff = (m_line, f_line)
+			if diff not in differences:
+				differences[diff] = 0
+			differences[diff] += 1
+
+	global_amount = 0
+	for amount in differences.values():
+		global_amount += amount
+
+	print(f'On average, every {round(len_f / global_amount)}th line changes')
+
+	print('differences:')
+	for diff, amount in sorted(differences.items(), key=lambda x:x[1], reverse=True):
+		m_line, f_line = diff
+		plural = 's' if amount > 1 else ''
+		print(f'  {amount} time{plural}: "{m_line}" becomes "{f_line}"')
+
+else:
+	print('the files have a different amount of lines, so this script will probably only output nonsense')
--- diffing/normalize for diffing.py
+++ diffing/normalize for diffing.py
@@ -96,20 +96,25 @@ with stay_open_on_error ():
 	master_static = 'masterbranch static.html'
 	feature_static = 'featurebranch static.html'
 
+# (NOTE) 	My PR removes so many of these invalid p's that the diff
+# (NOTE) 	becomes unreadable. So i remove them from the masterbranch
+# (NOTE) 	static export. Set to true to see these changes. BTW, the
+# (NOTE) 	normalized featurebranch static export is nearly HTML 5
+# (NOTE) 	conform, with some things to do left, which are subject
+# (NOTE) 	of other PR's (for example a's wrapping elements which
+# (NOTE) 	contain other a's, a div containing styles inside of the
+# (NOTE) 	head)
+	show_invalid_ps_in_master = False
+
+# (NOTE) 	Currently this will result in both files having the same
+# (NOTE) 	amount of lines, and they can then be compared using the
+# (NOTE) 	`log differences.py`
+	line_by_line_comparable = False
+
 	def norm(path):
 		print(f'Normalizing {path} ...')
 
 		is_feature = 'featurebranch' in path
-
-# (NOTE) 		My PR removes so many of these invalid p's that the diff
-# (NOTE) 		becomes unreadable. So i remove them from the masterbranch
-# (NOTE) 		static export. Set to true to see these changes. BTW, the
-# (NOTE) 		normalized featurebranch static export is nearly HTML 5
-# (NOTE) 		conform, with some things to do left, which are subject
-# (NOTE) 		of other PR's (for example a's wrapping elements which
-# (NOTE) 		contain other a's, a div containing styles inside of the
-# (NOTE) 		head)
-		show_invalid_ps_in_master = False
 
 		text = contents (realpath (path))
 
@@ -133,11 +138,11 @@ with stay_open_on_error ():
 		remove('<style>...</style>',
 			r'<style \s .+? </style>', regex.DOTALL
 		)
-
+		
 		remove('<script>...</script>',
 			r'<script \s .+? </script>', regex.DOTALL
 		)
-
+		
 		remove('<svg>...</svg>',
 			r'<svg \s .+? </svg>' , regex.DOTALL
 		)
@@ -159,7 +164,7 @@ with stay_open_on_error ():
 				&quot; (?!&quot;) .*? (?<!&quot;) &quot; (?!&quot;)	(*PRUNE)
 			''',regex.DOTALL
 		)
-
+		
 		remove('"" attributes from escaped html',
 			r'''
 				\s+ [$a-zA-Z] (?: -? [a-zA-Z0-9]+ )*	(*PRUNE)
@@ -167,7 +172,7 @@ with stay_open_on_error ():
 				&quot; \s* &quot; (?!&quot;)	(*PRUNE)
 			''', regex.DOTALL
 		)
-
+		
 		# (CLOSE)
 		# (OPEN) :Linebreak escapers
 		if is_feature:
@@ -176,17 +181,18 @@ with stay_open_on_error ():
 			)
 		# (CLOSE)
 		# (OPEN) :Remove empty p´s
-		if not is_feature:
-
-		# (NOTE) 	Those get eg. created by the navigator widget in the Tiddler
-		# (NOTE) 	'Creating SubStories'.
-			remove('empty p´s (only masterbranch)',
+		# (NOTE) Those get eg. created by the navigator widget in the Tiddler
+		# (NOTE) 'Creating SubStories'.
+		
+		if line_by_line_comparable or not is_feature:
+			only_masterbranch = '' if line_by_line_comparable else ' (only masterbranch)'
+			remove(f'empty p´s{only_masterbranch}',
 				r'<p> \s* </p>'
 			)
 		# (CLOSE)
 		# (OPEN) :Remove invalid p´s wrapping blockelements from the masterbranch wiki
-		if not is_feature and not show_invalid_ps_in_master:
-
+		if not line_by_line_comparable and not show_invalid_ps_in_master and not is_feature:
+		
 		# (NOTE) 	We can not do this using regexes because eg it will not catch (but
 		# (NOTE) 	should) ...
 		# (NOTE) 		<p>
@@ -194,9 +200,9 @@ with stay_open_on_error ():
 		# (NOTE) 		<div></div>
 		# (NOTE) 		</p>
 		# (NOTE) 	... so instead we use a mini parser.
-
+		
 			print('  Remove p´s we dont want from masterbranch wiki')
-
+		
 			blockelems = (
 				'(?:div|ul|ol|dl|table|tr|td|h[1-6]|p|pre|style|blockquote)'
 			)
@@ -204,7 +210,7 @@ with stay_open_on_error ():
 			p_locations = [0]
 			# (OPEN) :pat
 			pat = regex.compile(r'''
-
+			
 				(<\s*!\s*doctype\s+html\s*>)	# doctype – doctype tag
 				|
 				(<''' + blockelems + r'''>)	# O - blockelement opener
@@ -229,16 +235,16 @@ with stay_open_on_error ():
 			# (OPEN) :handler
 			def handler(match):
 				nonlocal cleaned, p_locations
-
+			
 				# See description in the pattern definition
 				doctype, O, C, S, v, o, c, s, other, error = match.groups()
-
+			
 				if O is not None:
 					cleaned.append(O)
 					cleaned[p_locations[-1]] = ''
 					if O == '<p>':
 						p_locations.append(len(cleaned)-1)
-
+			
 				elif C is not None:
 					if C == '</p>':
 						if cleaned[p_locations[-1]]:
@@ -246,22 +252,22 @@ with stay_open_on_error ():
 						p_locations.pop()
 					else:
 						cleaned.append(C)
-
+			
 				elif S is not None:
 					cleaned[p_locations[-1]] = ''
 					cleaned.append(S)
-
+			
 				elif v is not None: cleaned.append(v)
-
+			
 				elif error is not None:
 					raise Exception(error)
-
+			
 				elif doctype is not None: cleaned.append(doctype)
 				elif o is not None: cleaned.append(o)
 				elif c is not None: cleaned.append(c)
 				elif s is not None: cleaned.append(s)
 				elif other is not None: cleaned.append(other)
-
+			
 				return match[0]
 			# (CLOSE)
 			pat.sub(handler, text)
@@ -272,12 +278,13 @@ with stay_open_on_error ():
 		# (NOTE) the images are not wrapped in ps (which is ok, as the images are
 		# (NOTE) represented inline). I remove this change from the diff, because
 		# (NOTE) there are quite a lot of such, which disturbs the diffing.
-
-		replace("Remove wrapping p's from images in 'image-picker Macro' Tiddler",
-			r'<a>\s*<p>\s*<img>\s*</p>\s*</a>',
-			'<a><img></a>'
-		)
-
+		
+		if not line_by_line_comparable:
+			replace("Remove wrapping p's from images in 'image-picker Macro' Tiddler",
+				r'<a>\s*<p>\s*<img>\s*</p>\s*</a>',
+				'<a><img></a>'
+			)
+		
 		# (CLOSE)
 		# (OPEN) :Normalize whitespaces
 		replace('Compress all whitespace to " "', r'\s+', ' ')
